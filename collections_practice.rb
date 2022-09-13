08f706262c6a3c5d5307c55f6d71769777f9c61f
Yejin Na <101006019+JustEddie@users.noreply.github.com>
2022-09-14 02:33:44 +1000

    commit tree fe5653c35c9934b746117d7fcf5288cdbad555ac

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,58 @@
 
+def sort_array_asc(array)
+    array.sort
+end
+
+def sort_array_desc(array)
+    array.sort do |a, b|
+        if a == b
+          0
+        elsif a > b
+          -1
+        elsif a < b
+          1
+        end
+      end
+end
+
+def sort_array_char_count(array)
+    array.sort do |a, b|
+        if a == b
+          0
+        elsif a.length < b.length
+          -1
+        elsif a.length > b.length
+          1
+        end
+      end
+end
+
+def swap_elements(array)
+    array[1], array[2] = array[2], array[1]
+    array
+end
+
+def reverse_array(array)
+    array.reverse
+end
+
+def kesha_maker(array)
+    array.each {|word| word[2] = "$"}
+    array
+end
+
+def find_a(array)
+    array.select {|word| word[0]=="a"}
+end
+
+def sum_array(array)
+    result = 0
+    array.each {|num| result += num}
+    result
+end
+
+def add_s(array)
+    array.each_with_index.map {|word,index| index==1? word : word+"s"}
+
+
+end
